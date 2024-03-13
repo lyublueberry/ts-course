3063bd48a0dfab0ba7f751aac5da2d3aaeb78d4f
Lara87 <lara.87@bk.ru>
2024-03-13 09:16:32 +0300

    упражнение функция дженерик

---
+++ test8.ts
@@ -0,0 +1,20 @@
+function toString<T>(data: T): string | undefined {
+    if(Array isArray(data)) {
+        retrun data.toString();
+    }
+    switch (typeof data) {
+        case 'string':
+            return data;
+        case 'number':
+        case 'symbol':
+        case 'bigint':
+        case 'boolean':
+        case 'function':
+            return data.toString();
+        case 'object':
+            return JSON.stringify(data);   
+        default:
+            return undefined;
+    }
+    
+}
